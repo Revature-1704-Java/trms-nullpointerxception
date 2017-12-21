74a43f316a026bbf7b990aa2594bfd09f2f413e6
Steven Sagun <sagunsteven@live.com>
2017-12-21 13:30:43 -0500

    Added Employee bean.

---
+++ src/main/java/com/revature/bean/Employee.java
@@ -0,0 +1,102 @@
+package com.revature.bean;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+
+	private int employeeId;
+	private int departmentId;
+	private String username;
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String country;
+	private int reportsTo;
+	private boolean isBenCo;
+
+	public Employee() {
+		super();
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public int getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public int getReportsTo() {
+		return reportsTo;
+	}
+
+	public void setReportsTo(int reportsTo) {
+		this.reportsTo = reportsTo;
+	}
+
+	public boolean isBenCo() {
+		return isBenCo;
+	}
+
+	public void setBenCo(boolean isBenCo) {
+		this.isBenCo = isBenCo;
+	}
+
+}
